4ba3541c47206b8f4bf82925e8d3d9b84b99662f
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2025-11-26 00:57:24 +0530

    finished basic project setup with themes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+    <>
+        <div className="w-full h-full flex items-center justify-center">
+            <div className="bg-surface border border-border h-[64px] w-[64px] rounded-2xl">
+                <p>Hello  World</p>
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default App;

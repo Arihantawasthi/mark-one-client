6e9903ce015850f0dc633323594276b3a3f1b1d6
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2026-01-10 16:05:43 +0530

    feat: added user signature in nav; feat: added logout in user signature

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { CirclePlus } from "lucide-react";
+import { CirclePlus, LogOut, User } from "lucide-react";
 
 import NavHistoryList from "./NavHistoryList";
 import NavHistoryLoading from "./NavHistoryLoading";
@@ -8,11 +8,13 @@ import NavHistoryEmpty from "./NavHistoryEmpty";
 
 import { useAnalysesList } from "./useAnalysesList";
 import useAppContext from "../../context/useAppContext";
+import { useAuth } from "../../context/AuthProvider";
 
 
 function Navbar() {
     const { analyses } = useAppContext();
     const { isLoading, error, refetch } = useAnalysesList();
+    const { auth, logout } = useAuth();
     const navigate = useNavigate();
 
     const handleNewAnalysis = () => {
@@ -32,33 +34,42 @@ function Navbar() {
 
     return (
         <aside
-            className="w-64 fixed left-0 z-40 h-full bg-surface border-r border-border transition-all duration-300 ease-in-out flex flex-col"
+            className="w-64 fixed left-0 z-40 h-full bg-surface border-r border-border transition-all duration-300 ease-in-out flex flex-col justify-between"
         >
-            <div className="px-4 py-5 border-b border-border flex items-center justify-between">
-                <div className="flex items-center space-x-4 font-bold text-lg cursor-pointer" onClick={() => navigate("/")}>
-                    <div
-                        className="w-8 h-8 rounded flex items-center justify-center text-on-primary font-bold bg-primary-500"
-                    >
-                        T
+            <div>
+                <div className="px-4 py-5 border-b border-border flex items-center justify-between">
+                    <div className="flex items-center space-x-4 font-bold text-lg cursor-pointer" onClick={() => navigate("/")}>
+                        <div
+                            className="w-8 h-8 rounded flex items-center justify-center text-on-primary font-bold bg-primary-500"
+                        >
+                            T
+                        </div>
+                        <span>Thrive Letter</span>
                     </div>
-                    <span>Thrive Letter</span>
+                </div>
+
+                <div className="p-4">
+                    <button
+                        onClick={handleNewAnalysis}
+                        className="w-full flex items-center justify-center space-x-2 text-on-primary px-4 py-2 rounded-xl font-bold
+                                  transition-transform active:scale-95 bg-primary-500 cursor-pointer hover:bg-primary-600 ease-in-out"
+                    >
+                        <CirclePlus size={16} />
+                        <span>New Analysis</span>
+                    </button>
+                </div>
+
+                <div className="flex-1 overflow-y-auto px-2 py-2 space-y-1">
+                    <p className="px-4 text-[12px] font-bold text-zinc-500 mb-2 uppercase tracking-wider">History</p>
+                    { content }
                 </div>
             </div>
-
-            <div className="p-4">
-                <button
-                    onClick={handleNewAnalysis}
-                    className="w-full flex items-center justify-center space-x-2 text-on-primary px-4 py-2 rounded-xl font-bold
-                              transition-transform active:scale-95 bg-primary-500 cursor-pointer hover:bg-primary-600 ease-in-out"
-                >
-                    <CirclePlus size={16} />
-                    <span>New Analysis</span>
-                </button>
-            </div>
-
-            <div className="flex-1 overflow-y-auto px-2 py-2 space-y-1">
-                <p className="px-4 text-[12px] font-bold text-zinc-500 mb-2 uppercase tracking-wider">History</p>
-                { content }
+            <div className="flex justify-between items-center p-4 border-t border-b border-t-border border-b-border text-sm">
+                <span className="flex gap-x-2 items-center opacity-90">
+                    <User size={40} className="p-2 bg-border/80 text-white rounded-full" />
+                    <span className="font-medium">{/* Username */}{ auth.username }</span>
+                </span>
+                <LogOut size={20} className="cursor-pointer hover:text-red-500 transition" onClick={logout} />
             </div>
         </aside>
     );
